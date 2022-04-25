bb05aafe3c1011fd425597cb141c69b6e65492bc
kose-yusuke <yusuke188alc@icloud.com>
2022-04-25 15:43:19 +0900

    second commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 (module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://yusuke-ops.github.io/pythonlocalsetting',
+  url: 'https://kose-yusuke.github.io/pythonlocalsetting',
   baseUrl: '/pythonlocalsetting/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
